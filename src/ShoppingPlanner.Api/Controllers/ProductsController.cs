cfd1546eef5932b2607d465fb60aa3f55ef684f0
Izabella German <izzigerman@gmail.com>
2026-05-24 14:41:22 +0200

    feat: add ProductsController with CRUD endpoints

---
+++ src/ShoppingPlanner.Api/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingPlanner.Api.Dtos;
+using ShoppingPlanner.Api.Services;
+
+namespace ShoppingPlanner.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+    {
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+        {
+        _productService = productService;
+        }
+
+    // GET api/products
+    [HttpGet]
+    public ActionResult<IEnumerable<ProductDto>> GetAll()
+        {
+        var products = _productService.GetAll();
+        return Ok(products);
+        }
+
+    // GET api/products/5
+    [HttpGet("{id:int}")]
+    public ActionResult<ProductDto> GetById(int id)
+        {
+        var product = _productService.GetById(id);
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+        }
+
+    // POST api/products
+    [HttpPost]
+    public ActionResult<ProductDto> Create(CreateProductDto dto)
+        {
+        var product = _productService.Create(dto);
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = product.Id },
+            product);
+        }
+
+    // PUT api/products/5
+    [HttpPut("{id:int}")]
+    public ActionResult<ProductDto> Update(int id, UpdateProductDto dto)
+        {
+        var product = _productService.Update(id, dto);
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+        }
+
+    // DELETE api/products/5
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+        {
+        var deleted = _productService.Delete(id);
+        if (!deleted)
+            return NotFound();
+
+        return NoContent();
+        }
+    }
